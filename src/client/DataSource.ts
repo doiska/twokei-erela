a98af436c1c97ee7b705b1aea7e992179b2b2b6c
doiská <doiskka@gmail.com>
2022-08-04 16:31:05 -0300

    Added Caching

--- src/client/DataSource.ts
+++ src/client/DataSource.ts
@@ -1,5 +1,5 @@
-import { Guild } from "@entities/Guild";
-import { User } from "@entities/User";
+import { Guild } from "@models/Guild";
+import { User } from "@models/User";
 
 import { DataSource } from "typeorm";
 
@@ -11,6 +11,9 @@ export const dataSource = new DataSource({
 	ssl: true,
 	logging: true,
 	entities: [Guild, User],
+	cache: {
+		duration: 1000 * 60 * 60,
+	},
 	subscribers: [],
 	migrations: [],
 });
