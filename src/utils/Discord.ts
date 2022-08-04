79fc24e1e695bf9c682ccfec7b2c2e710e39c1f6
doiská <doiskka@gmail.com>
2022-08-04 16:08:42 -0300

    Removed some useless functions

--- src/utils/Discord.ts
+++ src/utils/Discord.ts
@@ -1,21 +1,52 @@
-import { Guild, ChannelType, ActionRowBuilder, Message } from "discord.js";
+import {
+	Guild,
+	ChannelType,
+	Message,
+	MessageOptions,
+	TextBasedChannel,
+	EmbedData,
+	EmbedBuilder,
+	SelectMenuBuilder,
+	ButtonBuilder,
+	ActionRowBuilder, InteractionReplyOptions, MessageComponentInteraction, ModalSubmitInteraction, NonThreadGuildBasedChannel
+} from "discord.js";
 
 import Twokei from "@client/Twokei";
 
 import { MessageActionRowComponentBuilder } from "@discordjs/builders";
 
-export const getChannelById = (id: string, guild?: Guild) =>
-	guild ? guild.channels.fetch(id) : Twokei.channels.fetch(id);
 
-export const getMessageById = async (id: string, channelId: string): Promise<Message | undefined> => {
-	const result = await getChannelById(channelId);
+export const getChannelById = async (id: string, guild: Guild): Promise<TextBasedChannel | undefined> => {
+	return guild.channels.fetch(id).then(c => c as TextBasedChannel).catch(() => undefined);
+}
 
-	if (!result || result.type !== ChannelType.GuildText) return;
+export const sendTemporaryMessage = (channel: TextBasedChannel, message: MessageOptions, timeout = 5000) => {
+	channel.send(message).then(m => {
+		setTimeout(() => {
+			m.delete().catch(() => {
+				// ignore
+			});
+		}, timeout)
+	});
+}
 
-	return result.messages.fetch(id);
-};
+export const reply = (interaction: ModalSubmitInteraction | MessageComponentInteraction, reply: InteractionReplyOptions, timeout = 5000) => {
+	interaction.reply(reply).then(() =>
+		setTimeout(() => interaction.deleteReply(), timeout)
+	)
+}
 
-export const parseBuilderToComponent = (builder: MessageActionRowComponentBuilder | MessageActionRowComponentBuilder[]) => {
-	const components = Array.isArray(builder) ? builder : [builder];
-	return new ActionRowBuilder<MessageActionRowComponentBuilder>().setComponents(...components);
-};
+export const builderToComponent = (builder: SelectMenuBuilder | ButtonBuilder | ButtonBuilder[]) => {
+	const component = Array.isArray(builder) ? builder : [builder];
+	return new ActionRowBuilder<MessageActionRowComponentBuilder>().setComponents(...component);
+}
+
+export const embed = (embedOptions?: string | EmbedData) => {
+	const embed = typeof embedOptions === "string" ? { description: embedOptions } : embedOptions;
+	return new EmbedBuilder(embed)
+}
+
+export const fastEmbed = (embedOptions?: string | EmbedData) => {
+	const builder = embed(embedOptions);
+	return { embeds: [builder] }
+}
